eb849fdd74057db645bb4fe8e47d7504b8d71a37
Poseidon <122105735+poseidonxy@users.noreply.github.com>
2023-06-23 11:05:28 +0100

    save high score in file

--- scoreboard.py
+++ scoreboard.py
@@ -7,12 +7,13 @@ ALIGNMENT = "center"
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
-        self.high_score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
@@ -26,4 +27,7 @@ class Scoreboard(Turtle):
     def reset_score(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.score}")
         self.score = 0
+        self.update_scoreboard()
